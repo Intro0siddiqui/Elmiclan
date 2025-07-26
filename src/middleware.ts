3474c390082aec28b081e44348fc9cfc57b2647e
Ali Siddiqui <alisiddiquichp@gmail.com>
2025-07-26 10:03:51 +0000

    I guess problem is with firebase server

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,19 +3,19 @@ import type { NextRequest } from 'next/server';
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  const sessionCookie = request.cookies.get('elmiclan-session'); // A placeholder for your actual session cookie
+  // const sessionCookie = request.cookies.get('elmiclan-user'); // A placeholder for your actual session cookie
 
   // Protect all dashboard routes
-  if (request.nextUrl.pathname.startsWith('/dashboard')) {
-    if (!sessionCookie) {
-      // Redirect unauthenticated users to the login page
-      return NextResponse.redirect(new URL('/', request.url));
-    }
+  // if (request.nextUrl.pathname.startsWith('/dashboard')) {
+  //   if (!sessionCookie) {
+  //     // Redirect unauthenticated users to the login page
+  //     return NextResponse.redirect(new URL('/', request.url));
+  //   }
 
     // Add more complex, rank-based logic here in the future
     // For example, decode the session cookie/token to get the user's rank
     // and redirect if they don't have access to a specific path.
-  }
+  // }
 
   return NextResponse.next();
 }
