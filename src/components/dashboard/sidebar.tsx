1d5095cc59b169d52fa248703db55fbad040e9de
Ali Siddiqui <alisiddiquichp@gmail.com>
2025-07-26 13:40:41 +0000

    Error: `legacyBehavior` is deprecated and will be removed in a future re

--- src/components/dashboard/sidebar.tsx
+++ src/components/dashboard/sidebar.tsx
@@ -64,7 +64,7 @@ export function DashboardSidebar() {
                       <SidebarMenuSub>
                         {item.subItems.map(subItem => (
                           <SidebarMenuSubItem key={subItem.href}>
-                              <Link href={subItem.href} legacyBehavior passHref>
+                              <Link href={subItem.href} passHref>
                                 <SidebarMenuSubButton asChild isActive={pathname === subItem.href}>
                                   <a>{subItem.label}</a>
                                 </SidebarMenuSubButton>
